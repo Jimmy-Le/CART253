b1d92355d2fc79d4f0982c6e4a2fceb21f00c58c
Jimmy <jimmy_le_@hotmail.com>
2026-09-14 23:45:11 -0400

    created a background using P5js shapes, and adjusted CSS background-color

--- instruction-challenge/js/script.js
+++ instruction-challenge/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Title of Project
- * Author Name
+ * IDK what the title is
+ * Piploop
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * PLEASE GIVE ME A BETTER GRADE FOR MY WORK IF IT'S GRADED!
  */
 
 "use strict";
@@ -12,7 +12,52 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(700, 700);
+    background(75,100,250);
 
+    // Flying creature
+
+
+    // More Tents
+    fill(200,240,255);
+    stroke(0,0,0);
+    triangle(200,350, 150,650, 450, 650)
+    triangle(500,350, 250,650, 650, 650)
+
+    // Hills
+    fill(255,255,255);
+    stroke(0,0,0);
+    ellipse(50, 500, 400, 600);
+    ellipse(650, 500, 400, 600);
+
+    // Sun
+    fill(255,255,0);
+    ellipse(650, 500, 100, 100);
+    fill(255,255,255);
+    ellipse(675, 475, 25, 25);
+
+    // Moon
+    fill(0,0,55);
+    ellipse(50, 500, 100, 100);
+    fill(255,255,255);
+    stroke(255,255,255);
+    ellipse(85, 450, 100, 100);
+    
+    // Tents
+    fill(200,240,255);
+    stroke(0,0,0);
+    triangle(350,350, 200,650, 500, 650)
+
+    // Tent Door
+    fill(0,50,150);
+    stroke(0,0,0);
+    triangle(350,550, 300, 650, 400, 650)
+
+
+    // Land
+    stroke(0,0,0);
+    fill(255,240,0);
+    ellipse(350, 700, 350, 200);
 }
 
 
@@ -20,5 +65,9 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+
+    
+
+
 
 }
