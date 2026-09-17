c3c98c20d0242501044dff03e47c5e124c9aab1b
Jimmy <jimmy_le_@hotmail.com>
2026-09-17 14:36:06 -0400

    added chest, body and arms

--- Assignment1/Prototype_1/js/script.js
+++ Assignment1/Prototype_1/js/script.js
@@ -14,6 +14,7 @@
 function setup() {
     createCanvas(500, 500);
     background(50,50,50);
+    strokeWeight(1);
 
 }
 
@@ -24,8 +25,19 @@ function draw() {
    
     // Body
     fill(150,200,255);
-    ellipse(250,275,175,175);
+    ellipse(250,275,175,150);
 
+
+    // Chest
+    fill(255,255,255);
+    ellipse(210,270,30,40);
+    ellipse(290,270,30,40);
+
+    // Arms
+
+    fill(150,200,255)
+    spline(185,210, 160,260, 165, 310, 220, 230);
+    spline(315,210, 340,260, 330, 310, 280, 230);
 
     generateHead();
 
@@ -41,8 +53,10 @@ function generateHead()
     fill(50,80,190);
     stroke(0,0,0);
     ellipse(250, 150, 205, 185);   
-    ellipse(220,210,80,100);
-    ellipse(280,210,80,100);        
+
+    // Neck
+    ellipse(220,210,70,100);
+    ellipse(280,210,70,100);        
     push();
 
     // Eye Circles
@@ -52,37 +66,37 @@ function generateHead()
     ellipse(305,160,80,120);
     ellipse(250,200,160,70);
     pop();
+
     // Eyes
     fill(0,0,0);
-    ellipse(195,150, 20, 40);
-    ellipse(305,150, 20, 40);
-
+    ellipse(195,170, 20, 40);
+    ellipse(305,170, 20, 40);
 
     // Eye Shine
     stroke(255,255,255);
     fill(255,255,255);
-    ellipse(200,140,15,15);
-    ellipse(310,140,15,15);
+    ellipse(200,160,15,15);
+    ellipse(310,160,15,15);
 
 
     // Nose
-    stroke(0,0,0)
+   
+    stroke(150,200,255)
     fill(150,200,255);
     beginShape();
 
     vertex(250, 140);       // Middle
     vertex(260, 160);
-    vertex(265, 140);       // Top Left
+    vertex(267, 140);       // Top Left
     vertex(265, 180);       // Bottom Left
     vertex(235, 180);       // Bottom Right
-    vertex(235, 140);       // Top Right
+    vertex(233, 140);       // Top Right
     vertex(240, 160);
     endShape(CLOSE);
-
+    stroke(0,0,0)
     // Beak
 
     fill(255,240,0);
     ellipse(250,190,40,35);
-
     line(230,190, 270,190);
 }
