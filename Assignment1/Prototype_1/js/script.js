ae10090a0ab9e42ed6957a3579f0e2b5f5d18aad
Jimmy <jimmy_le_@hotmail.com>
2026-09-16 21:15:11 -0400

    Moved Head Drawing to a generateHead function and started working on body

--- Assignment1/Prototype_1/js/script.js
+++ Assignment1/Prototype_1/js/script.js
@@ -22,11 +22,27 @@ function setup() {
 */
 function draw() {
    
+    // Body
+    fill(150,200,255);
+    ellipse(250,275,175,175);
+
+
+    generateHead();
+
+
+
+
+}
+
+function generateHead()
+{
     // Head
     
-    fill(50,80,180);
+    fill(50,80,190);
     stroke(0,0,0);
-    ellipse(250, 150, 200, 190);           
+    ellipse(250, 150, 205, 185);   
+    ellipse(220,210,80,100);
+    ellipse(280,210,80,100);        
     push();
 
     // Eye Circles
@@ -34,10 +50,10 @@ function draw() {
     stroke(255,255,255);
     ellipse(195,160,80,120);
     ellipse(305,160,80,120);
-    ellipse(250,200,150,65);
+    ellipse(250,200,160,70);
     pop();
     // Eyes
-    fill(70,80,200);
+    fill(50,80,200);
     ellipse(195,150, 20, 40);
     ellipse(305,150, 20, 40);
 
@@ -58,22 +74,22 @@ function draw() {
     
     // Nose
     
-    fill(80,135,255);
+    fill(150,200,255);
     beginShape();
 
-    vertex(250, 150);       // Middle
+    vertex(250, 140);       // Middle
     vertex(260, 160);
-    vertex(265, 150);       // Top Left
+    vertex(265, 140);       // Top Left
     vertex(265, 180);       // Bottom Left
     vertex(235, 180);       // Bottom Right
-    vertex(235, 150);       // Top Right
+    vertex(235, 140);       // Top Right
     vertex(240, 160);
     endShape(CLOSE);
-    
 
+    // Beak
 
+    fill(255,240,0);
+    ellipse(250,190,60,40);
 
-
-
-
+    line(220,190, 280,190);
 }
